2522f656444c6a157d2d285e2295375d406c08af
Bernardo <Bernardo@LAPTOP-TRRU5QEB>
2019-05-16 15:39:01 +0100

    web

--- src/main/webapp/js/ranking.js
+++ src/main/webapp/js/ranking.js
@@ -1,5 +1,5 @@
-var request = "5";
-var lastRequest = "0";
+var request = 5;
+var lastRequest = 0;
 var last = "";
 var values = {
     tokenID: localStorage.getItem('token'),
@@ -12,7 +12,7 @@ var values = {
 }
 
 captureDataR = function (values) {
-    
+
     console.log(JSON.stringify(values));
 
     $.ajax({
@@ -21,27 +21,43 @@ captureDataR = function (values) {
         contentType: "application/json;charset=utf-8",
         dataType: 'json', // data type        
         crossDomain: true,
-        success: function (Response) {},
-        error: function (Response) {
-            console.log(Response);
-            if (Response.status == 200) {
-                listDiv = document.getElementById('ranking');
-                var ul = document.createElement('ul');
-                for (var i = 0; i < data.length; ++i) {
-                    var li = document.createElement('li');
-                    li.innerHTML = data[i];
-                    ul.appendChild(li);
-                }
-                listDiv.appendChild(ul);
-                last = data[request - 1];
-                lastRequest = lastRequest + request;
-                console.log(last);
-                console.log(lastRequest);
+        success: function (Response) {
+            alert("aaaa");
+            var listDiv = document.getElementById('ranking');
+            var ul = document.createElement('ul');
+            for (var i = 0; i < data.length; ++i) {
+                var li = document.createElement('li');
+                li.innerHTML = data[i];
+                ul.appendChild(li);
             }
-
+            listDiv.appendChild(ul);
+            var vermais = document.createElement('button');
+            vermais.setAttribute("id", "vermais");
+            listDiv.appendChild(vermais);
+            last = data[request - 1];
+            lastRequest = lastRequest + request;
+            console.log(last);
+            console.log(lastRequest);
         },
+        error: function (Response) {},
         data: JSON.stringify(values) // post data || get data
     });
 };
 
-window.onload = captureDataR(values);
+window.onload = function captureDataR(values) {
+    var token = localStorage.getItem('expiration');
+    var date = new Date();
+    var longday = date.getTime();
+    if (longday > token) {
+        localStorage.clear();
+        window.location.href = "https://oofaround.appspot.com/";
+    } else {
+        setupCallback();
+    }
+};
+
+setupCallback = function (values) {
+    document.getElementById("vermais").addEventListener("click", function () {
+        captureDatar(values);
+    });
+};
