de3a75c9acf393a353b7d1074925b3db0e42b401
Salvas <Salvas@DESKTOP-J6IS52B>
2019-05-16 17:27:40 +0100

    Agora sim e de vez

--- src/main/java/pt/oofaround/resources/ImageResource.java
+++ src/main/java/pt/oofaround/resources/ImageResource.java
@@ -12,21 +12,23 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.google.cloud.storage.Acl;
-import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Acl.Role;
 import com.google.cloud.storage.Acl.User;
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
 import pt.oofaround.support.MediaSupport;
+import pt.oofaround.util.FolderData;
 import pt.oofaround.util.UploadImageData;
 
 @Path("/images")
 @Produces(MediaType.APPLICATION_JSON)
 public class ImageResource {
 
+	@SuppressWarnings("unused")
 	private static final Logger LOG = Logger.getLogger(ImageResource.class.getName());
 
 	private static final String BUCKET = "oofaround.appspot.com";
@@ -44,39 +46,21 @@ public class ImageResource {
 		return Response.ok().build();
 	}
 
+	@SuppressWarnings("unused")
 	@POST
 	@Path("/folder")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response createFolder() {
+	public Response uploadToProfileFolder(FolderData data) {
 		StorageOptions storage = StorageOptions.getDefaultInstance().toBuilder().setProjectId("oofaround").build();
 
 		Storage db = storage.getService();
 
-		BlobId blobId = BlobId.of(BUCKET, "BBB/");
+		BlobId blobId = BlobId.of(BUCKET, data.username + "/" + data.photoName);
 		BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
 				.setAcl(new ArrayList<>(Arrays.asList(Acl.of(User.ofAllUsers(), Role.READER)))).build();
-		
-		Blob blob = db.create(blobInfo);
+
+		Blob blob = db.create(blobInfo, data.image);
 
 		return Response.ok().build();
 	}
-
-	/*
-	 * @GET
-	 * 
-	 * @Path("{name}")
-	 * 
-	 * @Produces(MediaType.APPLICATION_JSON) public Response
-	 * downloadImage(@PathParam("name") String name) { Blob blob =
-	 * storage.get(BlobId.of(BUCKET, name)); return
-	 * Response.ok().entity(blob.getSize()).build(); }
-	 *
-	 * @GET
-	 * 
-	 * @Path("/logo") public Response downloadImage() { // Blob blob =
-	 * storage.get(BlobId.of(BUCKET, "logo_equipa.jpg")); Blob blob = db.get(BUCKET,
-	 * "logo_equipa.jpg", BlobGetOption.fields(Storage.BlobField.values())); String
-	 * s = "https://storage.googleapis.com/oofaround.appspot.com/" +
-	 * blob.getMediaLink(); return Response.ok(s).build(); }
-	 */
 }
---
+++ src/main/java/pt/oofaround/util/FolderData.java
@@ -0,0 +1,18 @@
+package pt.oofaround.util;
+
+public class FolderData {
+
+	public String username;
+	public String photoName;
+	public byte[] image;
+
+	public FolderData() {
+	}
+
+	public FolderData(String username, String photoName, byte[] image) {
+		this.username = username;
+		this.photoName = photoName;
+		this.image = image;
+	}
+
+}
