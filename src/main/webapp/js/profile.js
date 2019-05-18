228a9f6399b34a38e1007c6732cd2d6dde137b88
Bernardo <Bernardo@LAPTOP-TRRU5QEB>
2019-05-18 18:31:24 +0100

    web

--- src/main/webapp/js/profile.js
+++ src/main/webapp/js/profile.js
@@ -30,8 +30,7 @@ captureDataR = function () {
 
 var user = localStorage.getItem('username');
 window.onload = function () {
-    var $ANSWER = 'cat';
-    //document.getElementById("profilePic").src = 'https://storage.googleapis.com/oofaround.appspot.com/' + localStorage.getItem('username') + '_perfil';
+    document.getElementById("profilePic").src = 'https://storage.googleapis.com/oofaround.appspot.com/' + localStorage.getItem('username') + '_perfil';
     document.getElementById("profilePic").src = "https://oofaround.appspot.com/img/silhouette-profile-pic-profile-user-silhouette-318-40557.jpg"
     document.getElementById("user").innerHTML = user;
     var token = localStorage.getItem('expiration');
