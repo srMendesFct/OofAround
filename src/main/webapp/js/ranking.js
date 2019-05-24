91ceeefec9d920f3392e0a0262da3c713533baa6
Paulo Mimoso <p.mimoso@campus.fct.unl.pt>
2019-05-24 15:37:17 +0100

    y

--- src/main/webapp/js/ranking.js
+++ src/main/webapp/js/ranking.js
@@ -19,26 +19,28 @@
          contentType: "application/json;charset=utf-8",
          dataType: 'json', // data type        
          crossDomain: true,
-         success: function (Response) {
+         success: function (response) {
+        	 console.log(response);
              var listDiv = document.getElementById('ranking');
              var ul = document.createElement('ul');
-             ul.className = "container table table-borderless table-dark";
-             for (var i = 0; i < Response.scores.length; ++i) {
+             ul.className = "container table table-danger";
+             for (var i = 0; i < response.scores.length; ++i) {
                  var li = document.createElement('li');
                  li.className = "row";
                  var a = document.createElement('a');
-                 a.className = "col-xs-12 col-sm-6";
-                 var user = Response.scores[i].username;
-                 var score = Response.scores[i].score;
+                 var user = response.scores[i].username;
+                 user.className = "col-xs-12 col-sm-6";
+                 var score = response.scores[i].score;
+                 score.className = "col-xs-12 col-sm-6";
                  a.innerHTML = user + "    " + score;
                  li.appendChild(a);
                  ul.appendChild(li);
              }
              listDiv.appendChild(ul);
-             last = Response.scores[request - 1].username;
+             last = response.scores[request - 1].username;
              lastRequest = lastRequest + request;
          },
-         error: function (Response) {},
+         error: function (response) {},
          data: JSON.stringify(values) // post data || get data
      });
  };
