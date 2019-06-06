40b2f40ff25ca7f6472e6b4400959e42a71f51d9
Bernardo <Bernardo@LAPTOP-TRRU5QEB>
2019-06-06 14:30:32 +0100

    w

--- src/main/webapp/js/logout.js
+++ src/main/webapp/js/logout.js
@@ -12,6 +12,7 @@ captureDataG = function () {
             dataType: 'json', // data type        
             crossDomain: true,
             success: function (Response) {
+                console.log(Response.image);
                 localStorage.setItem('image', Response.image);
                 console.log(localStorage.getItem('image', Response.image));
             },
@@ -22,9 +23,9 @@ captureDataG = function () {
 };
 
 var user = localStorage.getItem('username');
-var image = localStorage.getItem('image');
 window.onload = function init() {
     captureDataG();
+    var image = localStorage.getItem('image');
     document.getElementById("profilePic").src = 'data:image/jpeg;base64, ' + image;
     document.getElementById("user").innerHTML = user;
     var token = localStorage.getItem('expiration');
