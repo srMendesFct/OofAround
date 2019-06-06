5ea6f6345dfaf305ae0349bbd675d99fbb69b9c3
Paulo Mimoso <p.mimoso@campus.fct.unl.pt>
2019-06-06 11:57:51 +0100

    frontend stuff

---
+++ src/main/webapp/js/route.js
@@ -0,0 +1,41 @@
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDiPZQSt7k2Hhi9YvZnpHl8ibIaL81fb24
+&callback=initMap"></script>
+
+function initMap() {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: {lat: 41.85, lng: -87.65}
+    });
+    directionsDisplay.setMap(map);
+
+    var onChangeHandler = function() {
+      calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+  };
+
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: document.getElementById('start').value,
+      destination: document.getElementById('end').value,
+      waypoints: [
+        {
+            location: 'gallup, nm',
+            stopover: false
+        },
+        {
+            location: 'barstow, ca',
+             stopover: true
+        }],
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  };
