d4c9adfc819fa68bfd15b477bbc9be528a8e4cef
Paulo Mimoso <p.mimoso@campus.fct.unl.pt>
2019-06-14 16:12:20 +0100

    percursos do crl

---
+++ src/main/webapp/js/create_route.js
@@ -0,0 +1,29 @@
+captureDataMonuments = function() {
+    var values = { 
+        tokenID: localStorage.getItem('token'),
+        usernameR: localStorage.getItem('username'),
+        role: localStorage.getItem('role'),
+
+    }
+    $.ajax({
+        type: "POST",
+        url: "https://oofaround.appspot.com/rest/location/get",
+        contentType: "application/json;charset=utf-8",
+        dataType: 'json',
+        crossDomain: 'true',
+        success: function(response) {
+            console.log(response.locations[i].name);
+            var m = document.getElementById('map');
+            for(var i = 0; i < response.locations.length; i++) {
+                var pos = new google.maps.LatLng(locations[i].latitude, locations[i].longitude);
+                var marker = new google.maps.Marker({
+                    position: pos,
+                    map: map
+                });
+
+            }
+        },
+        error: function (response) {},
+        data: JSON.stringify(values)
+    });
+}
