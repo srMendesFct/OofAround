ea3a7fae79142abf902a40b0a18c0d7d29c64a46
Paulo Mimoso <p.mimoso@campus.fct.unl.pt>
2019-06-14 17:06:52 +0100

    hm

--- src/main/webapp/js/create_route.js
+++ src/main/webapp/js/create_route.js
@@ -1,3 +1,44 @@
+var map;
+
+function initMap() {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: {lat: 38.71667, lng: -9.13333}
+    });
+    directionsDisplay.setMap(map);
+
+    var onChangeHandler = function() {
+      calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+  }
+
+  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: document.getElementById('start').value,
+      waypoints: [
+      {
+      location: 'gallup, nm',
+      stopover: false
+      },
+      {
+      location: 'barstow, ca',
+       stopover: true
+      }],
+      destination: document.getElementById('end').value,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
 captureDataMonuments = function() {
     var values = { 
         tokenID: localStorage.getItem('token'),
@@ -16,7 +57,6 @@ captureDataMonuments = function() {
         dataType: 'json',
         crossDomain: 'true',
         success: function(response) {
-            var m = document.getElementById('map');
             for(var i = 0; i < response.locations.length; i++) {
                 console.log(response.locations[i].name);
                 var pos = new google.maps.LatLng(response.locations[i].latitude, response.locations[i].longitude);
