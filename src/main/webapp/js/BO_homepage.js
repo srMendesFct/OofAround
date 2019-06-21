52c72265a0a4f8eb71ae9207e6fd734adab3e5de
Bernardo <Bernardo@10.22.111.239>
2019-06-21 14:33:22 +0100

    web

--- src/main/webapp/js/BO_homepage.js
+++ src/main/webapp/js/BO_homepage.js
@@ -10,7 +10,7 @@ function openCity(evt, cityName) {
 
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
+  for (i = 0; i < tablinks.length; i++) { 
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
