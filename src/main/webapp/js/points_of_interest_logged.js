dfdf56afdaa7cdf96904029f0add08f4f60da249
Bernardo <Bernardo@10.22.111.239>
2019-06-24 09:10:44 +0100

    web

--- src/main/webapp/js/points_of_interest_logged.js
+++ src/main/webapp/js/points_of_interest_logged.js
@@ -2,10 +2,10 @@ captureDataGetPointsOfInterest = function (event) {
     var limit = 5;
     var lastName = "";
     var x = document.getElementsByClassName("ed");
-    var res = "";
-    for (i = 0; i < x.length; i++) {
+    var res = {}
+    for (var i = 0; i < x.length; i++) {
         if (x[i].checked) {
-            res[i] = x[i].value;
+            res = x[i].value;
         }
     }
     var values = {
@@ -15,7 +15,7 @@ captureDataGetPointsOfInterest = function (event) {
         limit: limit,
         lastName: lastName,
         region: document.getElementById("distrito").value,
-        categoriesGet: JSON.stringify(res)
+        categoriesGet: res
     };
     console.log(JSON.stringify(values));
     $.ajax({
@@ -25,10 +25,6 @@ captureDataGetPointsOfInterest = function (event) {
         dataType: 'json', // data type        
         crossDomain: true,
         success: function (Response) {
-            tabcontent = document.getElementsByClassName("dente");
-            for (i = 0; i < tabcontent.length; i++) {
-                tabcontent[i].style.display = "none";
-            }
             lastName = 0; //buscar o ultimo elemento
             alert("nice");
         },
@@ -48,17 +44,16 @@ window.onload = function () {
     var longday = date.getTime();
     document.getElementById("profilePic").src = 'data:image/jpeg;base64, ' + image;
     document.getElementById("user").innerHTML = user;
-    if (longday > token) {
+    /*if (longday > token) {
         localStorage.clear();
         window.location.href = "https://oofaround.appspot.com/";
     } else {
         localStorage.setItem('expiration', date.getTime() + 300000);
         setupCallback();
-    }
+    }*/
 };
 
 setupCallback = function () {
-
     var frmsl = $('form[name="categorias"]');
     frmsl[0].onsubmit = captureDataGetPointsOfInterest;
 
