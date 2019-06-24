290789909bfd3852951029dd9d9deac10395f04b
Bernardo <Bernardo@10.22.111.239>
2019-06-24 12:15:50 +0100

    salva

--- src/main/java/pt/oofaround/util/ChangePasswordData.java
+++ src/main/java/pt/oofaround/util/ChangePasswordData.java
@@ -7,15 +7,17 @@ public class ChangePasswordData {
 	public String tokenID;
 	public String role;
 	public String usernameR;
+	public String confirmPassword;
 
 	public ChangePasswordData() {
 	}
 	
-	public ChangePasswordData(String password, String tokenID, String role, String usernameR, String oldPassword) {
+	public ChangePasswordData(String password, String tokenID, String role, String usernameR, String oldPassword, String confirmPassword) {
 		this.password = password;
 		this.oldPassword = oldPassword;
 		this.tokenID = tokenID;
 		this.role = role;
 		this.usernameR = usernameR;
+		this.confirmPassword = confirmPassword;
 	}
 }
