e9a3c7b0f49bdde66650b0319fca6b589acb4f99
Bernardo <Bernardo@10.22.111.239>
2019-06-24 16:07:55 +0100

    web

--- src/main/webapp/js/points_of_interest_logged.js
+++ src/main/webapp/js/points_of_interest_logged.js
@@ -100,8 +100,107 @@ captureDataGetPointsOfInterest = function (event) {
                 button.style.marginBottom = "3px";
                 button.innerHTML = "Saber Mais";
                 p.appendChild(button);
+
+                var div_3 = document.createElement("div");
+                div_3.setAttribute("class", "md-form mb-5");
+                document.getElementById("berna_modal").appendChild(div_3);
+
+                var label = document.createElement("label");
+                label.setAttribute("data-error", "wrong");
+                label.setAttribute("data-success", "right");
+                label.innerHTML = Response.locations[i].name;
+                div_3.appendChild(label);
+
+                var br = document.createElement("br");
+                document.getElementById("berna_modal").appendChild(br);
+
+                var div_4 = document.createElement("div");
+                div_4.setAttribute("class", "md-form mb-4");
+                document.getElementById("berna_modal").appendChild(div_4);
+
+                var label_2 = document.createElement("label");
+                label_2.setAttribute("data-error", "wrong");
+                label_2.setAttribute("data-success", "right");
+                label_2.innerHTML = "Morada:";
+                div_4.appendChild(label_2);
+
+                var p_2 = document.createElement("p");
+                p_2.innerHTML = Response.locations[i].address;
+                div_4.appendChild(p_2);
+
+                var div_5 = document.createElement("div");
+                div_5.setAttribute("class", "md-form mb-4");
+                document.getElementById("berna_modal").appendChild(div_5);
+
+                var label_3 = document.createElement("label");
+                label_3.setAttribute("data-error", "wrong");
+                label_3.setAttribute("data-success", "right");
+                label_3.innerHTML = "Categoria:";
+                div_5.appendChild(label_3);
+
+                var p_3 = document.createElement("p");
+                p_3.innerHTML = Response.locations[i].category;
+                div_5.appendChild(p_3);
+
+                var div_6 = document.createElement("div");
+                div_6.setAttribute("class", "md-form mb-4");
+                document.getElementById("berna_modal").appendChild(div_6);
+
+                var label_4 = document.createElement("label");
+                label_4.setAttribute("data-error", "wrong");
+                label_4.setAttribute("data-success", "right");
+                label_4.innerHTML = "Latitude:";
+                div_6.appendChild(label_4);
+
+                var p_4 = document.createElement("p");
+                p_4.innerHTML = Response.locations[i].latitude;
+                div_6.appendChild(p_4);
+
+                var div_7 = document.createElement("div");
+                div_7.setAttribute("class", "md-form mb-4");
+                document.getElementById("berna_modal").appendChild(div_7);
+
+                var label_5 = document.createElement("label");
+                label_5.setAttribute("data-error", "wrong");
+                label_5.setAttribute("data-success", "right");
+                label_5.innerHTML = "Longitude:";
+                div_7.appendChild(label_5);
+
+                var p_5 = document.createElement("p");
+                p_5.innerHTML = Response.locations[i].longitude;
+                div_7.appendChild(p_5);
+
+                var div_8 = document.createElement("div");
+                div_8.setAttribute("class", "md-form mb-4");
+                document.getElementById("berna_modal").appendChild(div_8);
+
+                var label_6 = document.createElement("label");
+                label_6.setAttribute("data-error", "wrong");
+                label_6.setAttribute("data-success", "right");
+                label_6.innerHTML = "Região:";
+                div_8.appendChild(label_6);
+
+                var p_6 = document.createElement("p");
+                p_6.innerHTML = Response.locations[i].region;
+                div_8.appendChild(p_6);
+
+                var div_9 = document.createElement("div");
+                div_9.setAttribute("class", "md-form mb-4");
+                document.getElementById("berna_modal").appendChild(div_9);
+
+                var label_7 = document.createElement("label");
+                label_7.setAttribute("data-error", "wrong");
+                label_7.setAttribute("data-success", "right");
+                label_7.innerHTML = "Descrição:";
+                div_9.appendChild(label_7);
+
+                var p_7 = document.createElement("p");
+                p_7.innerHTML = Response.locations[i].description;
+                div_9.appendChild(p_7);
+
+
+                lastName = Response.locations[i].name;
             }
-            lastName = Response.locations[i].name;
             console.log(Response);
             alert("Pesquisa com Sucesso");
         },
