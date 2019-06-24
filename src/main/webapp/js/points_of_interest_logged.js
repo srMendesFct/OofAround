9e400f55900f48410cba2207467671b16938d4d6
Bernardo <Bernardo@10.22.111.239>
2019-06-24 14:39:12 +0100

    web

--- src/main/webapp/js/points_of_interest_logged.js
+++ src/main/webapp/js/points_of_interest_logged.js
@@ -43,26 +43,19 @@ captureDataGetPointsOfInterest = function (event) {
 
                 var header_2 = document.createElement("h4");
                 header_2.style.textAlign = "left";
-                header_2.innerHTML = "Morada:" + Response.locations[i].address;
+                header_2.style.font = "10px"
+                header_2.innerHTML = "Morada: " + Response.locations[i].address;
                 div_2.appendChild(header_2);
-
 
                 var header_3 = document.createElement("h4");
                 header_3.style.textAlign = "left";
-                header_3.innerHTML = "Latitude:" + Response.locations[i].latitude;
+                header_3.innerHTML = "Categoria: " + Response.locations[i].category;
                 div_2.appendChild(header_3);
-
 
                 var header_4 = document.createElement("h4");
                 header_4.style.textAlign = "left";
-                header_4.innerHTML = "Longitude:" + Response.locations[i].longitude;
+                header_4.innerHTML = "Região: " + Response.locations[i].region;
                 div_2.appendChild(header_4);
-
-
-                var header_5 = document.createElement("h4");
-                header_5.style.textAlign = "left";
-                header_5.innerHTML = "Região:" + Response.locations[i].region;
-                div_2.appendChild(header_5);
 
 
                 var p = document.createElement("p");
