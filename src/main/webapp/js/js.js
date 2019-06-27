af652e1de73534862449c3ad5cf387f8437e994f
Bernardo <Bernardo@LAPTOP-TRRU5QEB>
2019-06-27 14:15:49 +0100

    web

---
+++ src/main/webapp/js/js.js
@@ -0,0 +1,30 @@
+captureDataGetPointsOfInterest = function (event) {
+    var res = [];
+
+    var values = {
+        tokenID: localStorage.getItem('token'),
+        role: localStorage.getItem('role'),
+        usernameR: localStorage.getItem('username'),
+        categories: res
+    };
+    console.log(JSON.stringify(values));
+    $.ajax({
+        type: "POST",
+        url: "https://oofaround.appspot.com/rest/route/listall",
+        contentType: "application/json;charset=utf-8",
+        dataType: 'json', // data type        
+        crossDomain: true,
+        success: function (Response) {
+            alert("Pesquisa com Sucesso");
+        },
+        error: function (Response) {
+            alert('Falha ao Pesquisar');
+        },
+        data: JSON.stringify(values) // post data || get data
+    });
+    event.preventDefault();
+};
+
+window.onload = function () {
+    captureDataGetPointsOfInterest();
+}
