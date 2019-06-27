1461e12dffa75a4e6d383b2a45b4ef34438e654e
Paulo Mimoso <p.mimoso@campus.fct.unl.pt>
2019-06-27 19:38:44 +0100

    register fixed

--- src/main/webapp/js/auth.js
+++ src/main/webapp/js/auth.js
@@ -6,14 +6,6 @@ window.onload = function () {
     frmsr[0].onsubmit = captureDataRegister;
 };
 
-function isEmailValid(email) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
-        return (true);
-    }
-    alert("You have entered an invalid email address!");
-    return (false);
-}
-
 captureDataGetUserInfo = function (values) {
     var values = {
         tokenID: localStorage.getItem('token'),
@@ -71,9 +63,7 @@ captureDataRegister = function (event) {
         values[field.name] = field.value;
     });
 
-    var mail = values['email'];
 
-    if(isEmailValid(mail)) {
     $.ajax({
         type: "POST",
         url: "https://oofaround.appspot.com/rest/register/user",
@@ -94,10 +84,6 @@ captureDataRegister = function (event) {
         data: JSON.stringify(values) // post data || get data
     });
     event.preventDefault();
-    
-    } else {
-        alert('Por favor introduza um e-mail válido.');
-    }
 };
 
 captureDataLogin = function (event) {
