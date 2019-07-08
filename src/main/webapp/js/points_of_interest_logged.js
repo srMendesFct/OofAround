4defbdee689a0c9dda4313ff82c6afb834b7446c
Bernardo <Bernardo@LAPTOP-TRRU5QEB>
2019-07-08 18:16:46 +0100

    web

--- src/main/webapp/js/points_of_interest_logged.js
+++ src/main/webapp/js/points_of_interest_logged.js
@@ -1,3 +1,4 @@
+var l = 0;
 captureDataGetPointsOfInterest = function (event) {
     var limit = 5;
     var lastName = "";
@@ -26,22 +27,6 @@ captureDataGetPointsOfInterest = function (event) {
         dataType: 'json', // data type        
         crossDomain: true,
         success: function (Response) {
-            var img_3 = document.createElement("img");
-            img_3.src = "";
-            var label = document.createElement("label");
-            label.innerHTML ="";
-            var p_2 = document.createElement("p");
-            p_2.innerHTML = "";
-            var p_3 = document.createElement("p");
-            p_3.innerHTML = "";
-            var p_4 = document.createElement("p");
-            p_4.innerHTML = "";
-            var p_5 = document.createElement("p");
-            p_5.innerHTML = "";
-            var p_6 = document.createElement("p");
-            p_6.innerHTML = "";
-            var p_7 = document.createElement("p");
-            p_7.innerHTML = "";
 
             var tabcontent = document.getElementsByClassName("tabcontent");
             for (i = 0; i < tabcontent.length; i++) {
@@ -174,17 +159,23 @@ captureDataGetPointsOfInterest = function (event) {
                 div_8.setAttribute("class", "md-form mb-5");
                 div_7.appendChild(div_8);
 
-                label.setAttribute("data-error", "wrong");
-                label.setAttribute("data-success", "right");
-                label.innerHTML = Response.locations[i].name;
-                div_8.appendChild(label);
+                document.createElement("label").setAttribute("id", l);
+                document.getElementById(l).setAttribute("data-error", "wrong");
+                document.getElementById(l).setAttribute("data-success", "right");
+                document.getElementById(l).innerHTML = Response.locations[i].name;
+                div_8.appendChild(document.getElementById(l));
+
+                l = l + 1;
 
                 var br = document.createElement("br");
                 div_8.appendChild(br);
 
-                img_3.setAttribute("class", "imgXL");
-                img_3.src = 'data:image/jpeg;base64, ' + Response.locations[i].image;
-                div_8.appendChild(img_3);
+                document.createElement("img").setAttribute("id", l);
+                document.getElementById(l).setAttribute("class", "imgXL");
+                document.getElementById(l).src = 'data:image/jpeg;base64, ' + Response.locations[i].image;
+                div_8.appendChild(document.getElementById(l));
+
+                l = l + 1;
 
                 var br_2 = document.createElement("br");
                 div_7.appendChild(br_2);
@@ -199,8 +190,11 @@ captureDataGetPointsOfInterest = function (event) {
                 label_2.innerHTML = "Morada:";
                 div_9.appendChild(label_2);
 
-                p_2.innerHTML = Response.locations[i].address;
-                div_9.appendChild(p_2);
+                document.createElement("p").setAttribute("id", l);
+                document.getElementById(l).innerHTML = Response.locations[i].address;
+                div_9.appendChild(document.getElementById(l));
+
+                l = l + 1;
 
                 var div_10 = document.createElement("div");
                 div_10.setAttribute("class", "md-form mb-4");
@@ -212,8 +206,11 @@ captureDataGetPointsOfInterest = function (event) {
                 label_3.innerHTML = "Categoria:";
                 div_10.appendChild(label_3);
 
-                p_3.innerHTML = Response.locations[i].category;
-                div_10.appendChild(p_3);
+                document.createElement("p").setAttribute("id", l);
+                document.getElementById(l).innerHTML = Response.locations[i].category;
+                div_10.appendChild(document.getElementById(l));
+
+                l = l + 1;
 
                 var div_11 = document.createElement("div");
                 div_11.setAttribute("class", "md-form mb-4");
@@ -225,8 +222,11 @@ captureDataGetPointsOfInterest = function (event) {
                 label_4.innerHTML = "Latitude:";
                 div_11.appendChild(label_4);
 
-                p_4.innerHTML = Response.locations[i].latitude;
-                div_11.appendChild(p_4);
+                document.createElement("p").setAttribute("id", l);
+                document.getElementById(l).innerHTML = Response.locations[i].latitude;
+                div_11.appendChild(document.getElementById(l));
+
+                l = l + 1;
 
                 var div_12 = document.createElement("div");
                 div_12.setAttribute("class", "md-form mb-4");
@@ -238,8 +238,11 @@ captureDataGetPointsOfInterest = function (event) {
                 label_5.innerHTML = "Longitude:";
                 div_12.appendChild(label_5);
 
-                p_5.innerHTML = Response.locations[i].longitude;
-                div_12.appendChild(p_5);
+                document.createElement("p").setAttribute("id", l);
+                document.getElementById(l).innerHTML = Response.locations[i].longitude;
+                div_12.appendChild(document.getElementById(l));
+
+                l = l + 1;
 
                 var div_13 = document.createElement("div");
                 div_13.setAttribute("class", "md-form mb-4");
@@ -251,8 +254,11 @@ captureDataGetPointsOfInterest = function (event) {
                 label_6.innerHTML = "Região:";
                 div_13.appendChild(label_6);
 
-                p_6.innerHTML = Response.locations[i].region;
-                div_13.appendChild(p_6);
+                document.createElement("p").setAttribute("id", l);
+                document.getElementById(l).innerHTML = Response.locations[i].region;
+                div_13.appendChild(document.getElementById(l));
+
+                l = l + 1;
 
                 var div_14 = document.createElement("div");
                 div_14.setAttribute("class", "md-form mb-4");
@@ -264,8 +270,11 @@ captureDataGetPointsOfInterest = function (event) {
                 label_7.innerHTML = "Descrição:";
                 div_14.appendChild(label_7);
 
-                p_7.innerHTML = Response.locations[i].description;
-                div_14.appendChild(p_7);
+                document.createElement("p").setAttribute("id", l);
+                document.getElementById(l).innerHTML = Response.locations[i].description;
+                div_14.appendChild(document.getElementById(l));
+
+                l = l + 1;
 
                 lastName = Response.locations[i].name;
             }
