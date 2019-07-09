b3ef1d6af7f9e299d3ccbff300b5c5ebcacef7b2
Bernardo <Bernardo@LAPTOP-TRRU5QEB>
2019-07-09 12:02:17 +0100

    weeb

--- src/main/webapp/js/list_routes_logged.js
+++ src/main/webapp/js/list_routes_logged.js
@@ -25,6 +25,27 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
+function openTab(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="dente" and hide them
+    tabcontent = document.getElementsByClassName("dente");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 captureDataGetRoutes = function (event) {
     var x = document.getElementsByClassName("ed");
     var res = []
@@ -268,244 +289,6 @@ captureDataGetRoutes = function (event) {
         data: JSON.stringify(values) // post data || get data
     });
     event.preventDefault();
-};
-
-captureDataGetRoutesByUser_teste = function (event) {
-    var values = {
-        tokenID: localStorage.getItem('token'),
-        role: localStorage.getItem('role'),
-        usernameR: localStorage.getItem('username'),
-    };
-    $.ajax({
-        type: "POST",
-        url: "https://oofaround.appspot.com/rest/userinfo/routes",
-        contentType: "application/json;charset=utf-8",
-        dataType: 'json', // data type        
-        crossDomain: true,
-        success: function (Response) {
-            console.log(Response);
-            var tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-                tabcontent[i].style.display = "none";
-            }
-
-            for (i = 0; i < Response.routes.length; i++) {
-                var x = [];
-                var y = [];
-
-                for (j = 0; j < Response.routes[i].categories.length; j++) {
-                    var z = Response.routes[i].categories[j].category;
-
-                    if (z == "Sport") {
-                        z = "Desporto";
-                    } else if (z == "Culture") {
-                        z = "Cultura";
-                    } else if (z == "NightLife") {
-                        z = "Vida Noturna";
-                    } else if (z == "Leisure") {
-                        z = "Lazer";
-                    } else if (z == "Animal & Wildlife") {
-                        z = "Animais e Vida Selvagem";
-                    } else if (z == "Outdoor & Pets") {
-                        z = "Ar livre e Animais Domésticos";
-                    } else if (z == "Beach") {
-                        z = "Praias";
-                    } else if (z == "Food & Drink") {
-                        z = "Comes e Bebes";
-                    } else if (z == "Landscaping") {
-                        z = "Paisagens";
-                    } else if (z == "Religion") {
-                        z = "Religião";
-                    }
-
-                    x[j] = z;
-                }
-
-                for (j = 0; j < Response.routes[i].regions.length; j++) {
-                    var z = Response.routes[i].regions[j].region;
-
-                    y[j] = z;
-                }
-
-                var div = document.createElement("div");
-                div.style.marginLeft = "3px";
-                div.setAttribute("id", "my");
-                div.setAttribute("class", "tabcontent");
-                document.getElementById("berna").appendChild(div);
-
-                var div_2 = document.createElement("div");
-                div_2.style.textAlign = "center";
-                div.appendChild(div_2);
-
-                var header = document.createElement("h4");
-                header.innerHTML = Response.routes[i].name;
-                div_2.appendChild(header);
-
-                var header_2 = document.createElement("h4");
-                header_2.style.textAlign = "left";
-                header_2.style.fontSize = "16px"
-                header_2.innerHTML = "Rating: " + Response.routes[i].rating;
-                div_2.appendChild(header_2);
-
-                var header_3 = document.createElement("h4");
-                header_3.style.textAlign = "left";
-                header_3.style.fontSize = "16px"
-                header_3.innerHTML = "Categorias: " + x;
-                div_2.appendChild(header_3);
-
-                var header_4 = document.createElement("h4");
-                header_4.style.textAlign = "left";
-                header_4.style.fontSize = "16px"
-                header_4.innerHTML = "Região: " + y;
-                div_2.appendChild(header_4);
-
-                var p = document.createElement("p");
-                p.align = "right";
-                div_2.appendChild(p);
-
-                var img = document.createElement("img");
-                img.src = "img/edit-pencil-icon-vector-13483315.jpg";
-                img.setAttribute("class", "imgXS");
-                p.appendChild(img);
-
-                var img_2 = document.createElement("img");
-                img_2.src = "img/cross.jpg";
-                img_2.setAttribute("class", "imgXS");
-                p.appendChild(img_2);
-
-                var button = document.createElement("button");
-                button.setAttribute("data-toggle", "modal");
-                button.setAttribute("data-target", "#loginForm" + i);
-                button.style.marginBottom = "3px";
-                button.innerHTML = "Saber Mais";
-                p.appendChild(button);
-
-                var div_3 = document.createElement("div");
-                div_3.setAttribute("class", "modal fade");
-                div_3.setAttribute("id", "loginForm" + i);
-                div_3.setAttribute("tabindex", "-1");
-                div_3.setAttribute("role", "dialog");
-                div_3.setAttribute("aria-labelledby", "myModalLabel");
-                div_3.setAttribute("aria-hidden", "true");
-                document.getElementById("body").appendChild(div_3);
-
-                var div_4 = document.createElement("div");
-                div_4.setAttribute("class", "modal-dialog");
-                div_4.setAttribute("role", "document");
-                div_3.appendChild(div_4);
-
-                var div_5 = document.createElement("div");
-                div_5.setAttribute("class", "modal-content");
-                div_4.appendChild(div_5);
-
-                var div_6 = document.createElement("div");
-                div_6.setAttribute("class", "modal-header text-center");
-                div_5.appendChild(div_6);
-
-                var img_3 = document.createElement("img");
-                img_3.src = "img/logo.png";
-                div_6.appendChild(img_3);
-
-                var button_2 = document.createElement("button");
-                button_2.setAttribute("type", "button");
-                button_2.setAttribute("class", "close");
-                button_2.setAttribute("data-dismiss", "modal");
-                button_2.setAttribute("aria-label", "Close");
-                div_6.appendChild(button_2);
-
-                var span = document.createElement("span");
-                span.setAttribute("aria-hidden", "true");
-                span.innerHTML = "&times";
-                button_2.appendChild(span);
-
-                var form = document.createElement("form");
-                form.setAttribute("id", "login");
-                form.setAttribute("name", "login");
-                div_5.appendChild(form);
-
-                var div_7 = document.createElement("div");
-                div_7.setAttribute("class", "modal-body mx-3");
-                form.appendChild(div_7);
-
-                var div_8 = document.createElement("div");
-                div_8.setAttribute("class", "md-form mb-5");
-                div_7.appendChild(div_8);
-
-                var label = document.createElement("label");
-                label.setAttribute("data-error", "wrong");
-                label.setAttribute("data-success", "right");
-                label.innerHTML = Response.routes[i].name;
-                div_8.appendChild(label);
-
-                var br = document.createElement("br");
-                div_8.appendChild(br);
-
-                var br_2 = document.createElement("br");
-                div_7.appendChild(br_2);
-
-                var div_10 = document.createElement("div");
-                div_10.setAttribute("class", "md-form mb-4");
-                div_7.appendChild(div_10);
-
-                var label_3 = document.createElement("label");
-                label_3.setAttribute("data-error", "wrong");
-                label_3.setAttribute("data-success", "right");
-                label_3.innerHTML = "Categorias:";
-                div_10.appendChild(label_3);
-
-                var p_3 = document.createElement("p");
-                p_3.innerHTML = x;
-                div_10.appendChild(p_3);
-
-                var div_13 = document.createElement("div");
-                div_13.setAttribute("class", "md-form mb-4");
-                div_7.appendChild(div_13);
-
-                var label_6 = document.createElement("label");
-                label_6.setAttribute("data-error", "wrong");
-                label_6.setAttribute("data-success", "right");
-                label_6.innerHTML = "Região:";
-                div_13.appendChild(label_6);
-
-                var p_6 = document.createElement("p");
-                p_6.innerHTML = y;
-                div_13.appendChild(p_6);
-
-                var div_14 = document.createElement("div");
-                div_14.setAttribute("class", "md-form mb-4");
-                div_7.appendChild(div_14);
-
-                var label_7 = document.createElement("label");
-                label_7.setAttribute("data-error", "wrong");
-                label_7.setAttribute("data-success", "right");
-                label_7.innerHTML = "Descrição:";
-                div_14.appendChild(label_7);
-
-                var p_7 = document.createElement("p");
-                p_7.innerHTML = Response.routes[i].description;
-                div_14.appendChild(p_7);
-
-                var div_15 = document.createElement("div");
-                div_15.setAttribute("class", "md-form mb-4");
-                div_7.appendChild(div_15);
-
-                var label_8 = document.createElement("label");
-                label_8.setAttribute("data-error", "wrong");
-                label_8.setAttribute("data-success", "right");
-                label_8.innerHTML = "Criado por:";
-                div_15.appendChild(label_8);
-
-                var p_8 = document.createElement("p");
-                p_8.innerHTML = Response.routes[i].creatorUsername;
-                div_15.appendChild(p_8);
-            }
-
-        },
-        error: function (Response) {
-            alert('Falha ao Pesquisar');
-        },
-        data: JSON.stringify(values) // post data || get data
-    });
 };
 
 captureDataGetRoutesByUser = function () {
