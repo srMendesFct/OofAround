c1ce80040ea030b3abb8fd00a04d6d3c907ee078
Bernardo <Bernardo@LAPTOP-TRRU5QEB>
2019-07-09 12:37:29 +0100

    web

--- src/main/webapp/js/list_routes_logged.js
+++ src/main/webapp/js/list_routes_logged.js
@@ -1,4 +1,5 @@
 marosca = 100;
+
 function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -9,23 +10,7 @@ function openCity(evt, cityName) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
-function openTab(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="dente" and hide them
-    tabcontent = document.getElementsByClassName("dente");
+    tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
@@ -66,7 +51,7 @@ captureDataGetRoutes = function (event) {
         crossDomain: true,
         success: function (Response) {
 
-            tabcontent = document.getElementsByClassName("random");
+            tabcontent = document.getElementsByClassName("tabcontent");
             for (i = 0; i < tabcontent.length; i++) {
                 tabcontent[i].style.display = "none";
             }
@@ -304,6 +289,12 @@ captureDataGetRoutesByUser = function () {
         success: function (Response) {
             console.log(Response);
             var counter = 0;
+
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+
             for (i = 0; i < Response.routes.length; i++) {
                 if (localStorage.getItem('username') == Response.routes[i].creatorUsername) {
                     counter = counter + 1;
@@ -551,6 +542,7 @@ setupCallback = function () {
     });
 
     document.getElementById("byUser").addEventListener("click", function () {
+        openCity("event", 'random');
         captureDataGetRoutesByUser();
     });
 };
